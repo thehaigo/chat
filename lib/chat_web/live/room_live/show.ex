aa9e2a385019f81ae6761d822c2217001ddabc5a
thehaigo <sirannha@gmail.com>
2020-11-25 16:28:25 +0900

    create chat room

--- lib/chat_web/live/room_live/show.ex
+++ lib/chat_web/live/room_live/show.ex
@@ -1,21 +1,38 @@
 defmodule ChatWeb.RoomLive.Show do
   use ChatWeb, :live_view
-
   alias Chat.Rooms
+  alias Chat.Rooms.Message
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(%{"id" => id}, session, socket) do
+    user = Chat.Users.get_user_by_session_token(session["user_token"])
+    room = Rooms.get_room_with_messages!(id)
+    changeset = Rooms.change_message(%Message{user_id: user.id, room_id: room.id})
+    return_to = Routes.room_show_path(socket, :show, room)
+    {:ok,
+      socket
+      |> assign(:page_title, page_title(socket.assigns.live_action))
+      |> assign(:room, room)
+      |> assign(:messages, room.messages)
+      |> assign(:return_to, return_to)
+      |> assign(:changeset, changeset)
+      |> assign(:user, user)
+    }
   end
 
   @impl true
-  def handle_params(%{"id" => id}, _, socket) do
-    {:noreply,
-     socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:room, Rooms.get_room!(id))}
+  def handle_event("save", %{"message" => message_params}, socket)
+  do
+    case Rooms.create_message(message_params) do
+      {:ok, _message} ->
+        {:noreply, socket
+          |> put_flash(:info, "Message created successfully")
+          |> push_redirect(to: socket.assigns.return_to)
+        }
+      {:error, %Ecto.Changeset{} = changeset } ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
   end
 
   defp page_title(:show), do: "Show Room"
-  defp page_title(:edit), do: "Edit Room"
 end
